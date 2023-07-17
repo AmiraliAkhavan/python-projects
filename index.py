c94ebb1a49abf0f5a56a8c6b1eebcd229633a718
Amirali Akhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-07-17 23:52:26 +0330

    Revert "final code"

--- index.py
+++
@@ -1,7 +0,0 @@
-salary = float(input("Enter the employee's salary: "))
-
-insurance = salary * 0.07
-tax = salary * 1/100
-final_salary = salary - insurance - tax
-
-print("Employee's Salary: $", final_salary)
