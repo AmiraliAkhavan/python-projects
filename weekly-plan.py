076489b6f5e36e978e8c7407a75732041f22351e
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-07-18 23:47:36 +0330

    Create weekly-plan.py

---
+++ weekly-plan.py
@@ -0,0 +1,3 @@
+print("-" * 44, 'barname', "-" * 44)
+print('Monday\tTuesday\tWednesday\tThursday\tFriday\tSaturday\tSunday\n\n')
+print('Riazi\tPython\tLinux\t\tJava\t\tMemari\tData science\tFreeTime')
