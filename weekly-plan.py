5b7ebb83d85812fb75f68025dfb5f7b4f747ad49
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-07-24 15:53:41 +0330

    Update weekly-plan.py

--- weekly-plan.py
+++ weekly-plan.py
@@ -1,7 +1,3 @@
-# print("-" * 44, 'barname', "-" * 44)
-# print('Shanbe\tYek-shanbe\tDo-shanbe\tSe-shanbe\tchahar-shanbe\tpanj-shanbe\tjome\n\n')
-# print('Riazi\tPython\t\tLinux\t\tJava\t\tMemari\t\tData science\tFreeTime')
-
-x={1,2,3,4}
-y={1,2,3,4,5,7,8}
-print(x.issubset(y))
+print("-" * 44, 'barname', "-" * 44)
+print('Shanbe\tYek-shanbe\tDo-shanbe\tSe-shanbe\tchahar-shanbe\tpanj-shanbe\tjome\n\n')
+print('Riazi\tPython\t\tLinux\t\tJava\t\tMemari\t\tData science\tFreeTime')
